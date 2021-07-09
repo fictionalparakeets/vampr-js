cef7147aa6184bbbddc24f5f669e184ce82a70ba
fictionalparakeets <banksmatthew32@gmail.com>
2021-07-09 22:32:48 +0000

    stretch added. one test failing

--- vampire.js
+++ vampire.js
@@ -42,8 +42,55 @@ class Vampire {
   // For example:
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
-  closestCommonAncestor(vampire) {
 
+  numberOfVampiresFromInput(input) {
+    let count = 0;
+    let currentVamp = this;
+    while (currentVamp.creator && currentVamp.creator !== input) {
+      currentVamp = currentVamp.creator;
+      count += 1;
+    }
+    return count;
+  }
+
+
+  closestCommonAncestor(vampire) {
+    if (vampire === this) {
+      return this;
+    }
+
+    const thisArr = [];
+    let currentThisVamp = this;
+    while (currentThisVamp.creator) {
+      currentThisVamp = currentThisVamp.creator;
+      thisArr.push(currentThisVamp);
+    }
+    if (thisArr.length === 0) {
+      thisArr[0] = currentThisVamp;
+    }
+
+    const vampArr = [];
+    let currentVamp = vampire;
+    while (currentVamp.creator) {
+      currentVamp = currentVamp.creator;
+      vampArr.push(currentVamp);
+    }
+    if (vampArr.length === 0) {
+      vampArr[0] = currentVamp;
+    }
+    console.log('thisArr: ', thisArr);
+    console.log('vampArr: ', vampArr);
+    
+    // iterate over each array and find common keys
+    const commonAncestors = [];
+    for (const firstVampObj of thisArr) {
+      for (const secondVampObj of vampArr) {
+        if (firstVampObj.name === secondVampObj.name) {
+          commonAncestors.push(firstVampObj);
+        }
+      }
+    }
+    return commonAncestors[0];
   }
 }
 
